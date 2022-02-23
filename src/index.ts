c1b63d829d48f375fd3a0708ca237ac058b1c28d
PimBreugem <pim.breugem076@gmail.com>
2022-02-23 11:37:50 +0100

    Structural changes

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,27 @@
-function add(a: number, b: number) {
-    return a + b
+import {IntermediateState, Fun} from "./fun"
+import {Either} from "./either"
+import {Pair} from "./pair"
+
+// Coroutine monadic definition
+// The monadic definition of a coroutine. When used, the Coroutine takes a State (S) to perform
+// the operation on, it will result in either a NoResult or a Pair with the result of Action (A)
+// and a new state of (S).
+
+type CoroutineMonadic<State, Errored, Action> = Fun<State, Either<NoResult<State, Errored, Action>, Pair<Action, State>>>
+
+// Coroutine operations definitions
+interface CoroutineOperations<State, Errored, Action> {
+    Do: <State, Errored, Action>() => {  }
+    RepeatUntil: <State, Errored>(condition: (_:State) => boolean) => Coroutine<State, Errored, IntermediateState>
+    Wait: <State, Errored, Action>() => {  }
+    Any: <State, Errored, Action>() => {  }
+    All: <State, Errored, Action>() => {  }
+    Parallel: <State, Errored, Action>() => {  }
+    Concurrent: <State, Errored, Action>() => {  }
 }
 
-console.log(add(1,2))
+// Export the Coroutine type, which is a combination* of monadic definition and behaviour from Operation
+export type Coroutine<State, Errored, Action> = CoroutineMonadic<State, Errored, Action> & CoroutineOperations<State, Errored, Action>
 
-
-// RepeatUntil(s => s.Counter > 10,
-//     Wait(5).then(() =>
-//         Do(s => ({...s, Counter:s.Counter+1}))
-//     )
-// )
-//
-//
-//
-// Wait(5) direct.
-//
-//
-//
-//
-
-interface Fun<input, output> {
-    (_:input) : output,
-    then<finalOutput>(postProcess:Fun<output, finalOutput>) : Fun<input, finalOutput>
-}
-
-const then = <input, intermediateOutput, output>(
-    first:Fun<input,intermediateOutput>,second:Fun<intermediateOutput, output>) : Fun<input,output> =>
-    fun((input:input) => second(first(input)))
-
-const fun = <input,output>(f:(_:input) => output) : Fun<input,output> => {
-    const fDecorated = f as Fun<input,output>
-    fDecorated.then = function<finalOutput>(postProcess:Fun<output, finalOutput>) : Fun<input, finalOutput> {
-        return then(this, postProcess)
-    }
-    return fDecorated
-}
-
-
-interface
+// Type of No Result should either error or go ahead with the coroutine, but because of circularity this should be implemented in another way ToDO
+type NoResult<State, Errored, Action> = Either<Errored, CoroutineN<State, Errored, Action>>
